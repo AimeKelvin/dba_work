9de8f93c465290ed053f850a14904e569df84480
aimekelvin <kellykelvinaime@gmail.com>
2025-05-07 16:06:53 +0200

    Remove client-side code and configuration files, including React components, context, styles, and Vite configuration.

--- api/server.js
+++ api/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Routes
